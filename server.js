f1b0f309dbbcd96ad5098902087c3eb6f5eb8f64
remilebeau <remilebeau90@gmail.com>
2024-02-24 12:58:57 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3500;
+
+// connect to db
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cors
+app.use(cors(corsOptions));
+
+// routes
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
+
+// start server
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
